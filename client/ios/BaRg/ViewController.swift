5b3a930943bed5bdfb918ed74ba36309cf61325d
gr-rassadnikov <gr>
2023-08-20 14:36:02 +0300

    Example project, Modules, UIComponents, dependencies

---
+++ client/ios/BaRg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaRg
+//
+//  Created by Grigorii Rassadnikov on 20.08.2023.
+//
+
+import UIKit
+import Modules
+import UIComponents
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        let _ = Modules()
+    }
+}
---
+++ client/ios/Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Grigorii Rassadnikov on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
