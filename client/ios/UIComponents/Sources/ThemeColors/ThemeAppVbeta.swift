47b519b86098d80bebca2ddca0e190c6993a6e30
gr-rassadnikov <gr>
2023-08-22 21:45:15 +0300

    UIComponents package, Typography, Images, Themes

---
+++ client/ios/UIComponents/Sources/ThemeColors/ThemeAppVbeta.swift
@@ -0,0 +1,12 @@
+//
+//  ThemeAppVbeta.swift
+//  
+//
+//  Created by Grigorii Rassadnikov on 20.08.2023.
+//
+
+import UIKit
+
+public final class ThemeAppVbeta: ThemeProvider {
+    public static let bg = UIColor.blue
+}
